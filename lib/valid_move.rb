963f85326eef78e3b66f1d89fd5a17be0f575c5b
Luis Reyes <luisreyesxv@yahoo.com>
2020-04-29 05:55:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@
 def valid_move?(board, index)
 
   def position_taken?(board,index)
-    board[index] != " " && board[index] != "" && board[index] != nil
+    board[index] == " " || board[index] == "" || board[index] == nil
   end
  index.between?(0,8) && position_taken?(board,index) == true
 
